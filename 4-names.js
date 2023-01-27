9e5b6663d5d976184b0074e0b954df0288452a27
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-27 13:31:19 +0800

    finished concepts modules

---
+++ 4-names.js
@@ -0,0 +1,11 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const Peter = "Peter"
+const Sally = "Sally"
+const Anson = "Anson"
+
+console.log(module)
+
+module.exports = {Peter, Sally, Anson}
+
