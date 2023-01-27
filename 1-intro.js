1cee379812a155a0f8022fa5672541e7b5a8754a
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-27 11:13:03 +0800

    finish first node file

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if (amount > 10) {
+    console.log("Larger than 10");
+}else{
+    console.log("Small number");
+}
+
+console.log("Hello World")
--- app.js
+++
@@ -1,9 +0,0 @@
-const amount = 12
-
-if (amount > 10) {
-    console.log("Larger than 10");
-}else{
-    console.log("Small number");
-}
-
-console.log("Hello World")
